dc7bc594d8f4429c6a206366bec224d3534595f3
joeylmaalouf <joeylmaalouf@gmail.com>
2015-10-24 13:49:12 -0400

    filled out validation fn

--- integer-tree.py
+++ integer-tree.py
@@ -5,21 +5,31 @@ def validate(argv):
   """ Given the argument values, make sure that we have
       a tree depth, and that it's a valid integer.
   """
-  pass
+  # if no depth is provided, show the user how to do so
+  if len(argv) < 2:
+    print("Usage: {} <depth>".format(argv[0]))
+    sys.exit(1)
+
+  # if the depth isn't a valid int, error out
+  try:
+    return int(argv[1])
+  except ValueError:
+    print("Error: Tree depth must be a valid integer.")
+    sys.exit(1)
 
 
 def make_tree(depth):
   """ Given a tree depth, (recursively?) create an actual
       tree from it that follows the problem requirements.
   """
-  pass
+  return [None] * depth
 
 
 def display_tree(tree):
   """ Given the tree structure, display it with proper
       formatting.
   """
-  pass
+  print(tree)
 
 
 if __name__ == "__main__":
